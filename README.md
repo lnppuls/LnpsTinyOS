0d09c202f55d06295b559f5c7748d67f5f17a6e9
Lnppuls <lnppuls@qq.com>
2023-07-02 01:01:43 +0800

    增加从磁盘扇区加载到内存功能

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 一个基于riscv的操作系统
-## 参考视频完成一个玩具级的OS
+## **参考博客园文章《30天自制操作系统》完成一个玩具级的OS**
 
-
+## [主要参考链接：30天自制操作系统](https://www.cnblogs.com/flashsun/category/1560379.html?page=1)
 ## [参考链接1](https://www.bilibili.com/video/BV1Q5411w7z5)
 ## [参考链接2](https://zhuanlan.zhihu.com/p/552943306)
-## [参考链接3](https://www.cnblogs.com/flashsun/category/1560379.html?page=1)
+
 
--- src/mbr.asm
+++ src/mbr.asm
@@ -11,11 +11,80 @@ int 0x10
 ; 显存写数据
 mov ax,0xb800
 mov gs,ax
-mov byte [gs:0x00],'H'
-mov byte [gs:0x02],'e'
-mov byte [gs:0x04],'l'
-mov byte [gs:0x06],'l'
-mov byte [gs:0x08],'o'
+mov byte [gs:0x00],'m'
+mov byte [gs:0x02],'b'
+mov byte [gs:0x04],'r'
+
+; 读取磁盘第二扇区加载到内存
+mov eax,0x02
+mov bx,0x900    ; 写入内存地址，备用
+mov cx,4        ; 待写入的扇区数
+call read_disk
+jmp 0x900       ; 直接执行写入内存的代码
+
+; 读硬盘方法
+read_disk:
+	mov esi,eax	;备份
+	mov di,cx	;备份
+	
+;第一步，设置要读取的扇区数
+	mov dx,0x1f2
+	mov al,cl
+	out dx,al
+	mov eax,esi	;恢复
+	
+;第二步，设置LBA地址
+	mov cl,8
+	;0-7位写入0x1f3
+	mov dx,0x1f3
+	out dx,al
+	;8-15位写入0x1f4
+	mov dx,0x1f4
+	shr eax,cl
+	out dx,al
+	;16-23位写入0x1f5
+	mov dx,0x1f5
+	shr eax,cl
+	out dx,al
+	;24-27位写入0x1f6
+	mov dx,0x1f6
+	shr eax,cl
+	and al,0x0f	;lba的24-27位
+	or al,0xe0	;另外4位为1110，表示lba模式
+	out dx,al
+	
+;第三步，写入读命令
+	mov dx,0x1f7
+	mov al,0x20
+	out dx,al
+
+;第四步，检测硬盘状态
+.not_ready:
+	nop
+	in al,dx
+	and al,0x88	;第4位为1表示准备好，第7位为1表示忙
+	cmp al,0x08
+	jnz .not_ready
+	
+;第五步，读数据
+	mov ax,di
+	mov dx,256
+	mul dx
+	mov cx,ax
+	
+	mov dx,0x1f0
+	.go_on_read:
+		in ax,dx
+		mov [bx],ax
+		add bx,2
+		loop .go_on_read
+		ret
+	
+
+
+
+
+
 
 ; 启动区标识
 times 510-($-$$) db 0
